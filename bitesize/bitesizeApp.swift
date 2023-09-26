57b720292b7d6e67ae7a8af5f1265403c788129c
ugik <ugyparaptughessa@gmail.com>
2023-09-26 10:51:11 +0700

    Initial Commit

---
+++ bitesize/bitesizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitesizeApp.swift
+//  bitesize
+//
+//  Created by Paraptughessa Premaswari on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct bitesizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bitesizeUITests/bitesizeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bitesizeUITestsLaunchTests.swift
+//  bitesizeUITests
+//
+//  Created by Paraptughessa Premaswari on 26/09/23.
+//
+
+import XCTest
+
+final class bitesizeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
