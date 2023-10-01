b09581129b4fcf21f1fb6324b472d0a8c418fcaa
Michelle Annice <michelle.annice@ui.ac.id>
2023-10-01 18:56:31 +0700

    LMS

---
+++ bitesize/Models/LMSWeightGirls.swift
@@ -0,0 +1,29 @@
+//
+//  LMSWeightGirls.swift
+//  bitesize
+//
+//  Created by Michelle Annice on 01/10/23.
+//
+
+import Foundation
+
+struct LMSWeightGirls {
+    var month: Int
+    var l: Double
+    var m: Double
+    var s: Double
+}
+
+// Create instances of LMSWeightGirls and populate the array
+let lmsDataWeightGirls: [LMSWeightGirls] = [
+    LMSWeightGirls(month: 5, l: -0.0430, m: 6.8985, s: 0.12274),
+    LMSWeightGirls(month: 6, l: -0.0756, m: 7.2970, s: 0.12204),
+    LMSWeightGirls(month: 7, l: -0.1039, m: 7.6422, s: 0.12178),
+    LMSWeightGirls(month: 8, l: -0.1288, m: 7.9487, s: 0.12181),
+    LMSWeightGirls(month: 9, l: -0.1507, m: 8.2254, s: 0.12199),
+    LMSWeightGirls(month: 10, l: -0.1700, m: 8.4800, s: 0.12223),
+    LMSWeightGirls(month: 11, l: -0.1872, m: 8.7192, s: 0.12247),
+    LMSWeightGirls(month: 12, l: -0.2024, m: 8.9481, s: 0.12268)
+]
+
+// You can add more data rows as needed
